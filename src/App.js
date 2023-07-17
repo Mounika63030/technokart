a1b57aa0fb2190a519c27f06eabdc0b5612fe1bd
Mounika63030 <ramavathmounikag06@gmail.com>
2023-07-17 10:06:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [invoiceNumber, setInvoiceNumber] = useState('');
+  const [invoiceDate, setInvoiceDate] = useState('');
+  const [invoiceAmount, setInvoiceAmount] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <form onSubmit={handleSubmit} className='form-container'>
+        <h3 className='heading'>Add Invoice</h3>
+        <div className="form-group">
+          <label>Invoice Number:</label>
+          <input
+            type="text"
+            className="form-control"
+            value={invoiceNumber}
+            onChange={(e) => setInvoiceNumber(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Invoice Date:</label>
+          <input
+            type="date"
+            value={invoiceDate}
+            onChange={(e) => setInvoiceDate(e.target.value)}
+          />
+        </div>
+        <div >
+          <label>Invoice Amount:</label>
+          <input
+            type="number"
+
+            value={invoiceAmount}
+            onChange={(e) => setInvoiceAmount(e.target.value)}
+          />
+        </div>
+        <button type="submit" >Add Invoice</button>
+      </form>
     </div>
+
   );
 }
 
